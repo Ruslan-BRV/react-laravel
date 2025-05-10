92c57c2da49dd9f6c5cfdc5c98874266976b5e01
Ruslan-BRV <ruslan10897@mail.ru>
2025-05-10 21:11:43 +0300

    Добавил общие данные и сделал при удалении поста всплывающее сообщение

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -70,6 +70,8 @@ class PostController extends Controller
     public function destroy(Post $post)
     {
         $post->delete();
-        return redirect('/');
+        return redirect('/')->with(
+            'message', 'Post has been deleted.'
+        );
     }
 }
--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,12 +1,27 @@
-import { Link } from '@inertiajs/react';
+import { Link, usePage } from '@inertiajs/react';
 import Pagination from '@/components/pagination';
 import { useRoute } from 'ziggy-js';
+import { useState } from 'react';
 
 export default function Home({ posts }) {
     const route = useRoute();
+
+    const { flash } = usePage().props
+
+    const [ flashMsg, setFlashMsg ] = useState(flash.message);
+
+    setTimeout(()=> {
+        setFlashMsg(null)
+    }, 2000);
+
     return (
       <>
         <h1 className="title">Привет</h1>
+          { flashMsg && (
+              <div className="absolute top-24 right-6 bd-rose-500 p-2 rounded-md shadow-lg text-sm text-white bg-danger">
+                  {flashMsg}
+              </div>
+          )}
           <div>
               {posts.data.map(post => (
                   <div key={post.id} className="p-4 border-b">
